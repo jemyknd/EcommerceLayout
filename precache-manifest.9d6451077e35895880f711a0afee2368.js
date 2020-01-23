f353477009e9639b17cf869cc65e97d9fb4a7b59
jemy <kndjemy@gmail.com>
2020-01-23 04:26:25 +0300

    Updates

---
+++ precache-manifest.9d6451077e35895880f711a0afee2368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5677b1aa09b94023cefcec6a3c2f7de1",
+    "url": "/EcommerceLayout/index.html"
+  },
+  {
+    "revision": "ed9cfb218717cfe7f239",
+    "url": "/EcommerceLayout/static/css/2.209056d4.chunk.css"
+  },
+  {
+    "revision": "0289b91d51e646697439",
+    "url": "/EcommerceLayout/static/css/main.cd7d0bc0.chunk.css"
+  },
+  {
+    "revision": "ed9cfb218717cfe7f239",
+    "url": "/EcommerceLayout/static/js/2.321bf69f.chunk.js"
+  },
+  {
+    "revision": "b651d5533fc80509587379dd07cc06bd",
+    "url": "/EcommerceLayout/static/js/2.321bf69f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0289b91d51e646697439",
+    "url": "/EcommerceLayout/static/js/main.39c69419.chunk.js"
+  },
+  {
+    "revision": "a2ce19c36fe8e97f8727",
+    "url": "/EcommerceLayout/static/js/runtime-main.d2819667.js"
+  }
+]);
